ec53e56e851dfa614b2d42f5938b938c19bb9fc5
chenjingyu <chenjingyu@tsingj.com>
2023-09-18 15:20:30 +0800

    Add: Specify a username to attempt to login to the server

--- cmd/ssh/ssh.go
+++ cmd/ssh/ssh.go
@@ -13,11 +13,15 @@ func NewSshCommand() *cobra.Command {
 		Short: "Connect to the server through SSH protocol",
 		Long:  "Connect to the server through SSH protocol",
 		Run: func(cmd *cobra.Command, args []string) {
+			user, _ := cmd.Flags().GetString("user")
 			targetIp := args[0]
 			configuration := config.LoadConfig()
 			sshControl := ssh.NewSshController(targetIp, configuration)
-			sshControl.TryLogin()
+			sshControl.TryLogin(user)
 		},
 	}
+	sshCmd.Flags().StringP(
+		"user", "u", "", "Specify a username to attempt to login to the server,\n"+
+			"if the specified username does not exist, try logging in using that username")
 	return sshCmd
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module tryssh
 go 1.16
 
 require (
-    github.com/pkg/sftp v1.13.5
-    github.com/schwarmco/go-cartesian-product v0.0.0-20180515110546-d5ee747a6dc9
-    github.com/sirupsen/logrus v1.9.3
-    github.com/spf13/cobra v1.7.0
-    golang.org/x/crypto v0.10.0
-    gopkg.in/yaml.v3 v3.0.1
+	github.com/pkg/sftp v1.13.6
+	github.com/schwarmco/go-cartesian-product v0.0.0-20180515110546-d5ee747a6dc9
+	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.7.0
+	golang.org/x/crypto v0.13.0
+	gopkg.in/yaml.v3 v3.0.1
 )
