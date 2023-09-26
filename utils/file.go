0c7542de6936c5a7697343220729ce835a0763e7
Driver-C <driver_c@foxmail.com>
2023-09-26 15:44:02 +0800

    Opt: change config path for adapt to various context

--- utils/file.go
+++ utils/file.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"gopkg.in/yaml.v3"
 	"os"
+	"path/filepath"
 )
 
 const (
@@ -10,6 +11,18 @@ const (
 )
 
 func FileYamlMarshalAndWrite(path string, conf interface{}) bool {
+	// Create a directory if it does not exist
+	dirPath := filepath.Dir(path)
+	if _, err := os.Stat(dirPath); err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(dirPath, 0755); err != nil {
+				Logger.Fatalln("Directory creation failed: ", err)
+			}
+		} else {
+			Logger.Fatalln("An error occurred while searching for the directory: ", dirPath)
+		}
+	}
+
 	confData, err := yaml.Marshal(conf)
 	if err != nil {
 		Logger.Fatalln("Configuration file marshal failed: ", err)
